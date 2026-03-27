788e67fd6d45508187809c0b17fb87b9b620d237
Italovini223 <italovinicius2018@gmail.com>
2026-03-27 12:44:20 -0300

    Fix output assignment for perceptron prediction and error tracking

--- perceptron.py
+++ perceptron.py
@@ -47,6 +47,7 @@ while treinamento  <= 5:
                 y = 1
             else:
                 y = -1
+                
             if y != d:
                 erro = True;
                 numero_de_erros += 1
